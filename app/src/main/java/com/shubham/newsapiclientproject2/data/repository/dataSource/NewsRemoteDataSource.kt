b77f751bac584e54b41b74b6ef7c71de33a835dc
Shubham Shinde <shindesshubham96@gmail.com>
2022-02-15 15:22:19 +0530

    Solved some HEAD text appending errors of git

--- app/src/main/java/com/shubham/newsapiclientproject2/data/repository/dataSource/NewsRemoteDataSource.kt
+++ app/src/main/java/com/shubham/newsapiclientproject2/data/repository/dataSource/NewsRemoteDataSource.kt
@@ -6,10 +6,7 @@ import retrofit2.http.GET
 
 interface NewsRemoteDataSource {
 
-<<<<<<< HEAD
     suspend fun getTopHeadlines(country: String, page: Int): Response<APIResponse>
-=======
-    suspend fun getTopHeadlines(): Response<APIResponse>
->>>>>>> 6db6fd57e0a6286ef51ac320f8a271cee7f7920d
+
 
 }
